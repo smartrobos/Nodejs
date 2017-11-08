491fba574c51d49c515e4f7d40b8b513e83edc03
Smart Robos <robosinctech@gmail.com>
2017-11-08 18:22:03 +0530

    Delete callbackex1.js

--- console/callback/callbackex1.js
+++
@@ -1,9 +0,0 @@
-function printHello() {
-  console.log(Date(), "Hello I can be used as Call back in another function")
-}
-console.log(Date(), "Before calling settime");
-/*Below inbuilt settimeout takes first argument as "printHello" (this is the call back function)
-and second argument is time delay in milliseconds.
-The settimeout calls the function asynchronously after the delay of 3000ms.
-*/
-setTimeout(printHello, 3000);
