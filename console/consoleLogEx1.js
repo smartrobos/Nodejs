7248fa32a015a38f8dd22e16ea1107d4dbb22ee8
Smart Robos <robosinctech@gmail.com>
2017-11-08 18:17:57 +0530

    Create consoleLogEx1.js

---
+++ console/consoleLogEx1.js
@@ -0,0 +1,10 @@
+var count = 1;
+var txt = 'example';
+console.log('This is the %dst %s in this tutorial', count, txt);
+let id = 123;
+let name = 'Ram';
+let details = {
+    "age": 22,
+    "salary": 20000,
+};
+console.log('employee id %d name %s' + '\n' + 'and more details are age %j and salary %j', id, name, details.age, details.salary);
