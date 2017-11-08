4311645c8ebb6e53a8e0571567706c1abfd83489
Smart Robos <robosinctech@gmail.com>
2017-11-08 18:20:09 +0530

    Create consolemethods.js

---
+++ console/consolemethods.js
@@ -0,0 +1,5 @@
+var txt = 'print';
+console.log('console.log for logging data');
+console.info('console.info to %s information', txt);
+console.error('console.error to %s errors', txt);
+console.warn('console.warn to %s warnings', txt);
