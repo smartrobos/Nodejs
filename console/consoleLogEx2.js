593bad3a351c98b07bf0c3ccccff4eb90f1b68d3
Smart Robos <robosinctech@gmail.com>
2017-11-08 18:18:28 +0530

    Create consoleLogEx2.js

---
+++ console/consoleLogEx2.js
@@ -0,0 +1,5 @@
+var txt1 = 'smartcodelab';
+var txt2 = 'provides well organised';
+var txt3 = 'easy to understand';
+var txt4 = 'web building tutorials';
+console.log('This is %s which %s and', txt1, txt2, txt3, txt4);
