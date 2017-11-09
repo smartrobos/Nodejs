31d2165289b6e7e333a21f74c74b63f18f06363c
Smart Robos <robosinctech@gmail.com>
2017-11-09 14:47:30 +0530

    Create setinterval.js

---
+++ Global Objects/Global Functions/setinterval.js
@@ -0,0 +1,5 @@
+function printHelloWorld() {
+    console.log("Hello, welcome to the World of global objects in Node.js!");
+}
+//call the above function after 3 seconds repetitively 
+setInterval(printHelloWorld, 3000);
