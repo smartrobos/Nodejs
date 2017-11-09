02cadba113635eba2e22b8aa0f7fbea5788b691d
Smart Robos <robosinctech@gmail.com>
2017-11-09 14:39:56 +0530

    Create processmethods.js

---
+++ process/processmethods.js
@@ -0,0 +1,6 @@
+// Print the current directory
+console.log('Current directory: ' + process.cwd());
+// Print the process version
+console.log('Current version: ' + process.version);
+// Print the memory usage
+console.log(process.memoryUsage());
