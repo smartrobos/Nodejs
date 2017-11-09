bccaa0538e27591e7be28c62d3012d8489b25051
Smart Robos <robosinctech@gmail.com>
2017-11-09 14:46:34 +0530

    Create set-timeout.js

---
+++ Global Objects/Global Functions/set-timeout.js
@@ -0,0 +1,5 @@
+function printHelloWorld() {
+    console.log("Hello, welcome to the World of global objects in Node.js!");
+}
+// Now call above function after 3 seconds
+setTimeout(printHelloWorld, 3000);
