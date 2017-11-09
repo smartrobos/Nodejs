5ada3ed25ca09a309c2e7fd3d34441cfe5122932
Smart Robos <robosinctech@gmail.com>
2017-11-09 15:49:27 +0530

    Create module-ex.js

---
+++ Modules/Module-Scope/module-ex.js
@@ -0,0 +1,5 @@
+console.log("Module Filename : " + module.filename);
+console.log("Module Id : " + module.id);
+console.log("Module Loaded : " + module.loaded);
+console.log("Module Parent : " + module.parent);
+console.log("Module Paths : " + module.paths);
